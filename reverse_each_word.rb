36adb2cdd414642c63451a652ecf0783d7dbcc26
Stephen Peters <7speter@gmail.com>
2020-05-04 18:14:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   sentence_array = string.split(' ')
-  sentence_items_reversed = sentence_array.each { |string| "#{string}".reverse }
+  sentence_array.each { |string| "#{string}".reverse }
   sentence_items_reversed
 end
